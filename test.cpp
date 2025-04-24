01fb092291508f31b9744f5a541c1d47555512eb
sen46 <sw20050719@gmail.com>
2025-04-24 23:50:27 +0900

    first commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <cctype>
+#include <iostream>
+using namespace std;
+void check(bool b)
+{
+	if (b)
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+}
+int main()
+{
+	cout << check(isdigit(CharT, const locale &))
+}
