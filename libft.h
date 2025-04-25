05c8f1003996741745efaed5b6bfda69daec52b3
sen46 <sw20050719@gmail.com>
2025-04-25 23:52:03 +0900

    modify

--- libft.h
+++ libft.h
@@ -15,12 +15,15 @@
 
 #include <unistd.h>
 
+int		my_atoi(const char *str);
 void	ft_bzero(void *s, size_t len);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+void	*ft_memchr(const void *buf, int c, size_t count);
+int		ft_memcmp(const void *buf1, const void *buf2, size_t count);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t count);
 void	*ft_memset(void *s, int c, size_t n);
@@ -28,7 +31,8 @@ char	*ft_strchr(const char *str, int c);
 size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 int		ft_strlen(char *str);
-int	ft_strncmp(const char *str1, const char *str2, size_t count);
+int		ft_strncmp(const char *str1, const char *str2, size_t count);
+char	*ft_strnstr(const char *big, const char *small, size_t len);
 char	*ft_strrchr(const char *str, int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
