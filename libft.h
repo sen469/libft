2d572161e741c18b0dd9e17f3b5cb44282845ee5
sen46 <sw20050719@gmail.com>
2025-04-25 15:59:13 +0900

    added functions

--- libft.h
+++ libft.h
@@ -13,6 +13,13 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+#include <unistd.h>
+
 int	ft_isalpha(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif
